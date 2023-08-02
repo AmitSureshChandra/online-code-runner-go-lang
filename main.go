ece2d3383bae9d6f0382634cca6efb2acd075f64
Amit <akumar00029@gmail.com>
2023-08-02 22:55:57 +0530

    code clear

--- main.go
+++ main.go
@@ -7,27 +7,12 @@ import (
 )
 
 func main() {
-
 	http.HandleFunc("/api/v1/run", handler.HandleRun)
 
-	// Run the server on port 8080
+	// Run the server on port 8010
 	err := http.ListenAndServe(":8010", nil)
+	fmt.Println("server started on 8010")
 	if err != nil {
 		fmt.Println("Error starting the server:", err)
 	}
-
-	//content := "public class Solution {\n" +
-	//	"\n" +
-	//	"    public static void main(String[] args) {\n" +
-	//	"        for(int i=0; i< 10000; i++)" +
-	//	"			for(int j=0; j< 10000; j++)" +
-	//	"				System.out.println(\"Hello World\");\n" +
-	//	"    }\n" +
-	//	"\n" +
-	//	"}"
-	//
-	//input := "Amit"
-	//compiler := "jdk8"
-
-	//runner.Run(content, input, docker.GetContainerName(compiler))
 }
